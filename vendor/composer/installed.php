ae7f7fe663033e6f57ebb6cdf82f331f8b875408
kentakunkentakun <kenta.0602.2000@icloud.com>
2021-02-11 18:19:39 +0900

    require line-bot-sdk

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e4fb16d2de8fd0a040e62e729d125197b5a288de',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e4fb16d2de8fd0a040e62e729d125197b5a288de',
+    ),
+    'linecorp/line-bot-sdk' => 
+    array (
+      'pretty_version' => '6.2.0',
+      'version' => '6.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b9d9a0d889c4a83b4521327882bf60be38b476e3',
+    ),
+  ),
+);
