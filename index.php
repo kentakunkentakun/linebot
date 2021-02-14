9a636b36c9e5776c066447face602acbbf324511
kentakunkentakun <kenta.0602.2000@icloud.com>
2021-02-15 08:18:37 +0900

    しりとり

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
     // Composerでインストールしたライブラリを一括読み込み
+    require_once("./phpQuery-onefile.php");
     require_once __DIR__ . '/vendor/autoload.php';
 
     // アクセストークンを使いCurlHTTPClientをインスタンス化
@@ -14,9 +15,70 @@
     //署名をチェックし、正当であればリクエストをパースし配列へ、不正であれば例外処理
     $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 
+    //ユーザーが打った単語
+    
     foreach ($events as $event) {
         // メッセージを返信
+        $replyWord = $event->getText();
+
+        //wikipediaからユーザーが入力した単語の情報を取得
+        //$wordSource = phpQuery::newDocumentFile('https://dictionary.goo.ne.jp/word/' . $replyWord);
+        $h1Text = $wordSource->find('h1')->text();
+        $h1Yomi = $wordSource->find('h1')->find('.yomi')->text();
+        $detailText = $wordSource->find('.meaning:first')->find('li')->find('p')->text();
+        //$textUrl = 'https://dictionary.goo.ne.jp/word/' . $replyWord;
+
+        // ①header、contents内のテキスト要素を作成
+        $headerText = new LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\TextComponentBuilder($h1Text);
+        $headerText->setWeight('bold');
+        $headerText->setWrap(true);
+
+        // ②header用のBoxComponentBuilderを作成
+        $headerBox = new LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\BoxComponentBuilder('vertical', [$headerText]);　
+        $headerBox->setPaddingBottom("sm");
+        $headerBox->setPaddingTop('xl');
+
+        // body
+        $bodyText1 = new LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\TextComponentBuilder($detailText);
+        $bodyText1->setWrap(true);
+        $bodyText1->setColor("#666566");
+        $bodyText1->setSize("sm");
+
+        $bodyBox1 = new LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\BoxComponentBuilder('vertical', [$bodyText1]);
+
+        $bodyText2 = new LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\TextComponentBuilder('出典： デジタル大辞泉 (小学館) / goo国語辞書');
+        $bodyText2->setColor("#666566");
+        $bodyText2->setSize("xxs");
+        $bodyText2->setAlign("end");
+
+        $bodyBox2 = new LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\BoxComponentBuilder('vertical', [$bodyText2]);
+
+        $bodyBox3 = $bodyBox1 = new LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\BoxComponentBuilder('vertical', [$bodyBox1, $bodyBox2]);
+        $bodyBox3->setPaddingAll("xxl");
+
+        //footer
+        $footerText = new LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\TextComponentBuilder("次の文字は「" . substr("abcdef", -2, 1) . "」です。");
+        $footerText->setAlign("center");
+
+        $footerBox = new LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\BoxComponentBuilder('vertical', [$footerText]);
+        $footerBox->setSpacing("sm");
+        $footerBox->setFlex(0);
+        $footerBox->setPaddingAll("xs");
+        $footerBox->setPaddingTop("none");
+
+
+
+
+        // ③BubbleContainerBuilder生成
+        $container = new LINE\LINEBot\MessageBuilder\Flex\ContainerBuilder\BubbleContainerBuilder();
+        $container->setHeader($headerBox); // ②で作成したheaderを詰める
+        $container->setBody($bodyBox3);
+        $container->setFooter($footerBox);
+
+        $flexMessage = new LINE\LINEBot\MessageBuilder\FlexMessageBuilder($h1Text, $container);
+        //$bot->replyMessage($replyToken, $flexMessage);
+        // bodyはsetBody、footerはsetFooterで詰める
         $response = $bot->replyMessage(
-            $event->getReplyToken(), new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("かいそ")  
+            $event->getReplyToken(), $flexMessage
         );
     }
--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -14,24 +14,33 @@ class InstalledVersions
 private static $installed = array (
   'root' => 
   array (
-    'pretty_version' => 'dev-master',
-    'version' => 'dev-master',
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
     'aliases' => 
     array (
     ),
-    'reference' => 'e4fb16d2de8fd0a040e62e729d125197b5a288de',
+    'reference' => 'bcdcfa54e9c68508f06c19ae025a93af8823923e',
     'name' => '__root__',
   ),
   'versions' => 
   array (
     '__root__' => 
     array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
       'aliases' => 
       array (
       ),
-      'reference' => 'e4fb16d2de8fd0a040e62e729d125197b5a288de',
+      'reference' => 'bcdcfa54e9c68508f06c19ae025a93af8823923e',
+    ),
+    'electrolinux/phpquery' => 
+    array (
+      'pretty_version' => '0.9.6',
+      'version' => '0.9.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6cb8afcfe8cd4ce45f2f8c27d561383037c27a3a',
     ),
     'linecorp/line-bot-sdk' => 
     array (
